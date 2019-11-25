0f50cec4e9d0d7e0340708c0d3877e251dca65ee
Tim Newton <tim.newton@eduserve.us>
2019-11-25 11:54:28 -0800

    Lesson 176 complete with fixes for dynamic table row height for multi-line labels.

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,25 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hhey!"),
+        Message(sender: "a@b.com", body: "hello!"),
+        Message(sender: "1@2.com", body: "what's up?"),
+        Message(sender: "a@b.com", body: "I'm having a very long day reading Lorem Ipsum statements and translating them into French then English. Ugh!"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.dataSource = self
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 68
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -32,5 +46,19 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
             print ("Error signing out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        cell.label.text = messages[indexPath.row].body
+        
+        return cell
     }
 }
